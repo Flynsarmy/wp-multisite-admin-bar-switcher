e23526b86cc5bebeba763ecd39141ce453c3f67a
flynsarmy <flynsarmy@gmail.com>
2014-09-26 13:14:41 +1000

    Only show site names when filtering

--- assets/js/mabs_site_filter.js
+++ assets/js/mabs_site_filter.js
@@ -2,19 +2,35 @@ jQuery(document).ready( function($) {
 	$('#wp-admin-bar-mabs_site_filter').show();
 
 	$('#mabs_site_filter_text').keyup(function() {
+		var queryLength = $(this).val().length;
 		var searchQuery = new RegExp( $(this).val(), 'i');
 
-		// Hide letters and blogs
-		$('#wp-admin-bar-mabs-default li.mabs_letter.menupop, #wp-admin-bar-mabs-default li.mabs_blog.menupop').hide();
+		// Remove previous filter results
+		$('#wp-admin-bar-mabs-default > li.mabs_blog.menupop').remove();
 
-		$('#wp-admin-bar-mabs-default li.mabs_blog.menupop > a').each(function() {
-			var matched = $(this).text().search(searchQuery) !== -1;
+		// Hide letters if we're filtering, else show them
+		$('#wp-admin-bar-mabs-default li.mabs_letter.menupop').toggle( !queryLength );
 
-			if ( matched )
-			{
-				$(this).closest('li.mabs_blog.menupop').show();
-				$(this).closest('li.mabs_letter.menupop').show();
-			}
-		});
+		if ( queryLength )
+		{
+			$('#wp-admin-bar-mabs-default li.mabs_blog.menupop').each(function(index, elem) {
+				var matched = $('> a', elem).text().search(searchQuery) !== -1;
+
+				if ( matched )
+				{
+					var id = $(elem).attr('id') + '_' + index;
+					var clone = $(elem).clone().attr('id', id);
+
+					$('#wp-admin-bar-mabs-default').append(clone);
+					clone.hoverIntent({
+						over: function() {
+							$(this).addClass("hover")
+						}, out: function() {
+							$(this).removeClass("hover")
+						}
+					});
+				}
+			});
+		}
 	});
 });
